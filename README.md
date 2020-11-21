32de5b649c366ecf2d676bec818815215857fd0e
Kristian Wiklund <githubkristian@snabela.nl>
2020-11-21 12:05:04 +0100

    Supports "pause/restart" and nothing else, at the moment

---
+++ README.md
@@ -0,0 +1,8 @@
+This connects sonos to zigbee2mqtt to control speakers with IKEA whassname controller
+
+The controller need to be named after the speaker, e.g. mqtt topic <prefix>/<speaker>, then everything works out of the box.
+This is case sensitive
+
+<prefix> need to uniquely identify what messages are from controllers
+
+You probably need to set debounce on the controller, see https://www.zigbee2mqtt.io/devices/E1744.html
---
+++ zigbee2soco.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+
+#change this to fit your own prefix. I use the stereo part to automatically identify only stereo events
+
+mqttprefix="zigbee/stereo"
+
+import paho.mqtt.client as mqtt
+import soco
+import sys
+import traceback
+
+def discover():
+    zones = {x.player_name:x for x in  soco.discover()}
+    
+    print("ZONES: "+str(zones))
+    return zones
+
+def pause(zones, speaker):
+    print("Pausing "+speaker)
+    state = zones[speaker].get_current_transport_info()['current_transport_state']
+    print(state)
+
+    if state == "PLAYING":
+        zones[speaker].pause()
+    else:
+        zones[speaker].play()
+
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe(mqttprefix+"/+/action")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, zones, msg):
+    
+    print(msg.topic+" "+str(msg.payload))
+    
+    payload = msg.payload.decode("utf-8")
+    print(payload)
+
+    try:
+        topic = msg.topic
+        topic = topic.replace(mqttprefix+"/","")
+        topic = topic.replace("/action","")
+        print(topic)
+        print(zones)
+#    .removesuffix("/action")
+    except:
+        print(traceback.format_exc())
+        print (sys.exc_info()[0])
+
+
+    if not topic in zones:
+        print("No such speaker: "+topic)
+        return
+
+    if payload == "play_pause":
+        pause(zones,topic)
+
+        
+## do stuff
+
+zones = discover()
+    
+client = mqtt.Client(userdata=zones)
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("localhost", 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
